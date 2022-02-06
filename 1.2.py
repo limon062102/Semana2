01c91dc3d9728dbf6a3cdb0e61a99940c3a34ad1
gabriel-villabon <78036096+gabriel-villabon@users.noreply.github.com>
2022-02-06 17:32:16 -0500

    Add files via upload

---
+++ 1.2.py
@@ -0,0 +1,28 @@
+import numpy as np
+from celluloid import Camera
+import matplotlib.pyplot as plt
+
+def function(time,armonic):
+    f = (2/armonic)*(-1)**(armonic-1)*np.sin(armonic*time)
+    return f
+
+L = np.pi
+cicles = 2
+time = np.linspace(0,2*L*cicles,100)
+
+armonics_init = 1
+armonics_end = 50
+Function = 0
+
+figure = plt.figure(figsize=(6,6))
+camera = Camera(figure)
+
+while armonics_init < armonics_end+1:
+
+    Function += function(time,armonics_init)
+    plt.plot(time,Function,c="g")
+    camera.snap()
+    armonics_init += 1
+
+animation = camera.animate()
+animation.save("fourierFunction.gif")
